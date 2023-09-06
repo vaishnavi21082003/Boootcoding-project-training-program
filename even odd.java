fd9115a8760d18ca3e3f4a372119009202c7aee9
vaishnavi21082003 <144039415+vaishnavi21082003@users.noreply.github.com>
2023-09-06 17:21:44 +0530

    Update and rename even odd to even odd.java

---
+++ even odd.java
@@ -0,0 +1,14 @@
+class even odd{
+      public static void main(String[] args)
+	  {
+	    int number=23;
+		if(number%2 == 0)
+		{
+		  System.out.println(number+"is even number");
+		  }
+		  else
+		  {
+		  System.out.println(number+"is odd number");
+		  }
+		 }
+		}
